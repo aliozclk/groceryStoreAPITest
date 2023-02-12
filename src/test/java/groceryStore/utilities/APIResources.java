c23a2c8ee4cac5f4c106a86cdf66da6bfa8dc24f
aliozclk <alliozcelik96@gmail.com>
2023-02-12 14:38:29 +0300

    Grocery Store API Test

--- src/test/java/groceryStore/utilities/APIResources.java
+++ src/test/java/groceryStore/utilities/APIResources.java
@@ -1,23 +1,51 @@
 package groceryStore.utilities;
 
+import groceryStore.pojos.response.CreateCartResponse;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class APIResources {
 
     //elements that are needed in path
-    private String cartId;
+    private String cartId ;
     private String itemId;
     private String productId;
+    private String token;
 
     //paths
     private String statusAPI = "/status";
     private String getAllProductsAPI = "/products";
-    private String getSpecifiedProductAPI = "/products/"+ productId +":productId" ;
+    private String addItem ;
+
+
+
+
+    private String getSpecifiedProductAPI;
     private String createCartAPI = "/carts";
-    private String addItemToCartAPI = createCartAPI+"/"+cartId+"/items";
-    private String modifyAnItem = addItemToCartAPI + "/" + itemId ;
+    private String addItemToCartAPI ;
+    private String modifyAnItem ;
     private String getTokenAPI = "/api-clients";
     private String createAnOrderAPI = "/orders";
 
+    public void setGetSpecifiedProductAPI(String productId) {
+        this.getSpecifiedProductAPI = "/products/"+ productId +":productId" ;
+    }
+
+    public void setAddItemToCartAPI(String cartId) {
+        this.addItemToCartAPI = createCartAPI+"/" + cartId + "/items";
+    }
+
+    public void setModifyAnItem(String itemId) {
+        this.modifyAnItem = addItemToCartAPI + "/" + itemId ;
+    }
+
+    public void setAddItem() {
+        this.addItem = (getCreateCartAPI() +"/" + getCartId() +"/items");
+    }
 }
