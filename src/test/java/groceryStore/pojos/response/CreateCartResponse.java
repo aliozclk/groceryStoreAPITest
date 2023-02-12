5a0921d1d980471baf67353c9bb9f6250bcda157
aliozclk <alliozcelik96@gmail.com>
2023-02-12 14:38:54 +0300

    Grocery Store API Test

--- src/test/java/groceryStore/pojos/response/CreateCartResponse.java
+++ src/test/java/groceryStore/pojos/response/CreateCartResponse.java
@@ -1,9 +1,13 @@
 package groceryStore.pojos.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class CreateCartResponse {
 	private boolean created;
